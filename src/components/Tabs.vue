763e2c6e8be0ccbff929e81498b43b6a588bf9a1
Kasatkin <aleksey.kasatkin@saritasa.com>
2020-06-03 17:43:04 +0700

    Add styles for the page
    Add router.
    
    #AI-1

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
-    <div>
-      <img  class="header" src="/src/assets/img/header.png" alt="Altair Invest"/>
-    </div>
-
-    <div class="navbar-fixed-top" id="myHeader">
-      <md-tabs md-sync-route>
-        <md-tab id="tab-company" md-label="О компании">
+  <body>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header mdl-layout__header--waterfall portfolio-header">
+      <div class="mdl-layout__header-row portfolio-logo-row">
+                <span class="mdl-layout__title">
+                    <div class="portfolio-logo"></div>
+                </span>
+      </div>
+    </header>
+    <div class="mdl-layout__header-row portfolio-navigation-row">
+      <md-tabs md-sync-route class="mdl-navigation mdl-typography--body-1-force-preferred-font">
+        <md-tab id="tab-company" class="mdl-navigation__link" md-label="О компании">
         </md-tab>
-        <md-tab id="tab-flats" md-label="Квартиры">
+        <md-tab id="tab-flats" class="mdl-navigation__link" md-label="Квартиры">
         </md-tab>
-        <md-tab id="tab-contacts" md-label="Контакты">
+        <md-tab id="tab-contacts" class="mdl-navigation__link" md-label="Контакты">
         </md-tab>
       </md-tabs>
     </div>
+    <router-view></router-view>
+    <md-bottom-bar></md-bottom-bar>
+
+    <footer class="footer">
+      <div class="mdl-mini-footer__left-section">
+        <div class="mdl-logo">Simple portfolio website</div>
+      </div>
+      <div class="mdl-mini-footer__right-section">
+        <ul class="mdl-mini-footer__link-list">
+          <li><a href="#">Help</a></li>
+          <li><a href="#">Privacy & Terms</a></li>
+        </ul>
+      </div>
+    </footer>
   </div>
+  </body>
 </template>
 
 <script>
@@ -32,13 +51,5 @@
 </script>
 
 <style scoped>
-  .header {
-    width: 100%;
-  }
 
-  .navbar-fixed-top {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
-  }
 </style>
